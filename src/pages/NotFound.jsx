5d1763edade66abda1522a7deeae1757f8e980de
Sanid-Assara <sanid.assara@gmail.com>
2024-07-26 01:38:07 +0200

    Delete a duplicate layout folder

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <>
       {/* <div className="m-auto min-h-screen flex justify-center flex-wrap place-content-center bg-cyan-950"> */}
-      <div className="grid grid-col-2 place-content-center bg-cyan-950 py-10">
+      <div className="min-h-screen grid grid-col-2 place-content-center bg-cyan-950 py-10">
         <figure className="place-self-center w-2/3">
           <img src={errorImg} alt="Page not found" />
         </figure>
