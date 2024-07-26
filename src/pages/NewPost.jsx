4d82c9b0165539187a5a43e547a6167c85acd4ef
Sanid-Assara <sanid.assara@gmail.com>
2024-07-26 11:19:17 +0200

    UPDATE API URL and add cancel button to createPost

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -1,38 +1,40 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function NewPost() {
   const [form, setForm] = useState({
-    author:'',
-    title: '',
-    content: '',
-    cover: ''
+    author: "",
+    title: "",
+    content: "",
+    cover: "",
   });
 
   const navigate = useNavigate();
-  
 
   const handleSubmit = async () => {
     if (!form.title || !form.content || !form.author || !form.content) {
-      alert('All fields need to be filled in');
+      alert("All fields need to be filled in");
       return;
     }
 
     try {
-      const response = await fetch('http://localhost:5000/API/posts', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(form),
-      });
+      const response = await fetch(
+        "https://blogs-api-s835.onrender.com/API/posts",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(form),
+        }
+      );
 
       const data = await response.json();
       if (data.error) {
         alert(data.error);
       } else {
-        alert('Post created successfully!');
-        navigate('/');
+        alert("Post created successfully!");
+        navigate("/");
       }
     } catch (error) {
       console.log(error);
@@ -42,48 +44,55 @@ export default function NewPost() {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="bg-white p-4 rounded-lg shadow-2xl w-[400px] mt-[-10%]">
-        <p className="text-4xl font-bold mb-4 text-center p-10">Create Your New Post</p>
-        
+        <p className="text-4xl font-bold mb-4 text-center p-10">
+          Create Your New Post
+        </p>
+
         <div className="space-y-6">
-          <input 
+          <input
             className="border-gray-300 border rounded-lg w-full p-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            type="text" 
+            type="text"
             placeholder="Author"
-            value={form.author} 
-            onChange={(e) => setForm({ ...form, author: e.target.value })} 
+            value={form.author}
+            onChange={(e) => setForm({ ...form, author: e.target.value })}
           />
 
-          <input 
-            type="text" 
-            className="border-gray-300 border rounded-lg w-full p-1 focus:outline-none focus:ring-2 focus:ring-blue-500"   
+          <input
+            type="text"
+            className="border-gray-300 border rounded-lg w-full p-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
             placeholder="Title"
-            value={form.title}  
+            value={form.title}
             onChange={(e) => setForm({ ...form, title: e.target.value })}
           />
-          
-          <input 
+
+          <input
             className="border-gray-300 border rounded-lg w-full p-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            type="text" 
+            type="text"
             placeholder="Content"
-            value={form.content} 
-            onChange={(e) => setForm({ ...form, content: e.target.value })} 
+            value={form.content}
+            onChange={(e) => setForm({ ...form, content: e.target.value })}
           />
 
-           <input 
+          <input
             className="border-gray-300 border rounded-lg w-full p-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            type="text" 
+            type="text"
             placeholder="Cover"
-            value={form.cover} 
-            onChange={(e) => setForm({ ...form, cover: e.target.value })} 
+            value={form.cover}
+            onChange={(e) => setForm({ ...form, cover: e.target.value })}
           />
 
           <div className="flex justify-center mt-2">
-            <button 
-              onClick={handleSubmit} 
+            <button
+              onClick={handleSubmit}
               className="border-2 p-1 rounded-lg w-20 bg-blue-500  text-white hover:bg-blue-700 transition-colors"
             >
               Submit
             </button>
+            <Link to="/">
+              <button className="border-2 p-1 rounded-lg w-20 bg-red-500  text-white hover:bg-red-700 transition-colors">
+                Cancel
+              </button>
+            </Link>
           </div>
         </div>
       </div>
